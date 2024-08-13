f5db91a847f701f6ea5eed49f1cfa3f88e4f1e9d
kubilaycicek <kubilaycicek0693@gmail.com>
2024-08-14 00:54:08 +0300

    Added Apigateway, Eureka

--- customer-service/src/main/java/com/kubilaycicek/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/kubilaycicek/customer_service/CustomerServiceApplication.java
@@ -2,10 +2,12 @@ package com.kubilaycicek.customer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableCouchbaseRepositories
 public class CustomerServiceApplication {
--- customer-service/src/main/java/com/kubilaycicek/customer_service/api/controller/CustomerController.java
+++ customer-service/src/main/java/com/kubilaycicek/customer_service/api/controller/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(("api/v1/customer"))
+@RequestMapping("customer-service")
 public class CustomerController {
 
     private final CustomerService customerService;
